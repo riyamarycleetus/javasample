f969e2663d94beb5b450677e67b8dc8ec967d64c
Riya Mary Cleetus <121925520+riyamarycleetus@users.noreply.github.com>
2024-04-25 12:58:55 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,17 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        
+        int n = 10;
+
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(a + " ");
+
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+}
